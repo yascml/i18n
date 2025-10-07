59ceaa04641d5cc9b8b4b7c1d2958cd3290773fc
Misa Liu <misaliu@misaliu.top>
2025-10-07 19:45:36 +0800

    chore: Finishing Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import cp from 'vite-plugin-cp';
+import zipPack from 'vite-plugin-zip-pack';
+import { version } from './package.json';
 
 export default defineConfig(({ mode }) => ({
   build: {
@@ -11,4 +15,25 @@ export default defineConfig(({ mode }) => ({
       formats: [ 'umd' ]
     },
   },
+  plugins: [
+    cp({
+      targets: [
+        {
+          src: resolve(__dirname, './meta.json'), dest: resolve(__dirname, './dist'), 
+          transform(buf) {
+            const meta = JSON.parse(buf.toString());
+            return JSON.stringify({
+              ...meta,
+              version,
+            });
+          }
+        }
+      ],
+    }),
+    zipPack({
+      inDir: resolve(__dirname, './dist'),
+      outDir: resolve(__dirname, './dist'),
+      outFileName: 'i18n.zip',
+    }),
+  ],
 }));
